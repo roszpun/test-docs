bd5abfa72e6431a3936180e66c3bbef66d8dbea8
Daniel Szerszeń <szerszen.daniel@gmail.com>
2024-09-04 12:22:14 +0200

    test-3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Siteeeee test branch TEST 22222',
+  title: 'My Siteeeee test branch TEST 333333',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
